d4e028e1887612d2cd392d96b0ddb8d163eed785
Marcus Swanson <marcus.swanson91@gmail.com>
2023-06-05 23:09:14 +0200

    Improve version handling
    * Use the latest minor tag when creating the version.
    * Local builds gets '-local' appended to version.

--- scripts/generate_app_info.py
+++ scripts/generate_app_info.py
@@ -1,3 +1,5 @@
+import os
+import re
 import subprocess
 from datetime import datetime
 from string import Template
@@ -6,13 +8,16 @@ from common import get_repo_root
 
 
 def get_major_minor_rev():
-    proc = subprocess.run(['git', 'describe', '--abbrev=0'], check=True, capture_output=True, text=True)
-    tag = proc.stdout.strip()
+    proc = subprocess.run(['git', 'tag'], check=True, capture_output=True, text=True)
+    tags = proc.stdout.strip().splitlines()
+    pattern = re.compile('^\d+\.\d+$')
+    minor_tags = [tag for tag in tags if pattern.search(tag)]
+    minor_tag = minor_tags[-1]
 
-    proc = subprocess.run(['git', 'rev-list', f'{tag}..', '--count'], check=True, capture_output=True, text=True)
+    proc = subprocess.run(['git', 'rev-list', f'{minor_tag}..', '--count'], check=True, capture_output=True, text=True)
     commit_count_since_tag = proc.stdout.strip()
 
-    return f'{tag}.{commit_count_since_tag}'
+    return f'{minor_tag}.{commit_count_since_tag}'
 
 
 def get_branch():
@@ -43,7 +48,9 @@ def get_timestamp():
 def generate_version():
     version = get_major_minor_rev()
 
-    if (branch := get_branch()) != 'master':
+    if os.environ.get('DCMEDIT_RELEASE') != 'true':
+        version += '-local'
+    elif (branch := get_branch()) != 'master':
         version += f'-{branch}'
 
     commit_hash = get_commit_hash()
